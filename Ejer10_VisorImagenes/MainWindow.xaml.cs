1a3a63f2a85549b3127058ef3a894d03aa91d64b
abc <abc@example.com>
2020-11-03 19:25:11 +0100

    Agregar archivos de proyecto.

---
+++ Ejer10_VisorImagenes/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Ejer10_VisorImagenes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textoTextBlock.HorizontalAlignment = HorizontalAlignment.Right;
+        }
+    }
+}
